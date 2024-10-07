e99c5885e749a873c6d6c5e404bc2dde0dffb598
Hampus Olviken <hampus.olviken@gmail.com>
2024-10-07 10:53:09 +0200

    yml

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,16 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 
-test('TC1 Test login and see homepage', async ({ page }) => {
-  await page.getByRole('heading', { name: 'Tester Hotel Overview' }).click();
-  await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+
+
+test.describe('Frontend testsuite', () => {
+  test('testsuite 01', async ({ page }) => {
+    //inlogning
+    await page.goto("http://localhost:3000");
+    await page.locator('#app > div > form > div:nth-child(1) > input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    await page.locator('#app > div > form > div:nth-child(2) > input[type=password]').fill(`${process.env.TEST_PASSWORD}`);
+    await page.locator('#app > div > form > div.field.action > button').click();
+
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible()
+    await page.getByRole('heading', { name: 'Tester Hotel Overview' }).click();
+  })
+
 });
 
-// test.describe("backend", ()=>{
-//   test('testcase-nr01', async ({ page }) => {
-//     await page.goto('http://localhost:3000');
-//   })
 
-// });
 
 
